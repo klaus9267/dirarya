b56bc9bf8cf1bbdebc245e8749213177b8ba6f4f
Kim minho <klaus9267@gmail.com>
2024-03-20 03:20:18 +0900

    Feat: 생성, 수정 날짜 필드 추가

--- src/main/java/grouf/dirarya/domain/user/User.java
+++ src/main/java/grouf/dirarya/domain/user/User.java
@@ -1,6 +1,8 @@
 package grouf.dirarya.domain.user;
 
+import grouf.dirarya.domain.common.TimeEntity;
 import grouf.dirarya.domain.user.dto.JoinDto;
+import grouf.dirarya.domain.user.dto.UpdateUserDto;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,7 +17,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User extends TimeEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
@@ -27,5 +29,10 @@ public class User {
         .password(hashedPassword)
         .nickname(joinDto.nickname())
         .build();
+  }
+
+  public void updateUser(final UpdateUserDto updateUserDto) {
+    this.nickname = updateUserDto.getNickname().isEmpty() ? this.nickname : updateUserDto.getNickname();
+    this.password = updateUserDto.getPassword().isEmpty() ? this.password : updateUserDto.getPassword();
   }
 }
