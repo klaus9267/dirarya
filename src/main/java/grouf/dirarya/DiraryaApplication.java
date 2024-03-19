923be57415c7398045462b9a4cd925f33976506d
Kim minho <klaus9267@gmail.com>
2024-03-20 00:05:15 +0900

    Docs: initial commit

---
+++ src/main/java/grouf/dirarya/DiraryaApplication.java
@@ -0,0 +1,13 @@
+package grouf.dirarya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiraryaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiraryaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/grouf/dirarya/DiraryaApplicationTests.java
@@ -0,0 +1,13 @@
+package grouf.dirarya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiraryaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
